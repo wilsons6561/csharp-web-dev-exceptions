ccaea7850f132a35f97fc2b75b1f6c41ff727508
wilsons6561 <wilsons6561@users.noreply.github.com>
2022-09-28 11:24:25 -0500

    Finished exercises

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,21 +7,48 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+        
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0.0)
+            {
+                throw new ArgumentOutOfRangeException("y", "You cannot divide by zero!")
+            } else 
+            { 
+            return x/y;
+                }
+
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            if(fileName == null || fileName == "")
+            {
+                throw new ArgumentNullException("filename", "Student did not submit any work");
+            } else
+            {
+                if(fileName.Substring(fileName.Length - 3, 3) == ".cs")
+                {
+                    return 1;
+                } else
+                {
+                    return 0;
+                }
+            }
         }
-        */
+        
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+
+            try
+            {
+                Divide(num1, num2);
+            } catch(ArgumentOutOfRangeException, e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -30,7 +57,11 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
-
+            try { 
+            CheckFileExtension(students["Brad"])
+                    } catch(ArgumentNullException, e){
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
